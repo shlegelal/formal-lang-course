77c7f1c7d12cf651426401db28ab1fae369e9d23
shlegelal <shlegel.alink0@gmail.com>
2022-10-02 11:28:12 +0300

    Comment dataset tests

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -36,41 +36,41 @@ def test_generate_and_export_graph():
     )
 
 
-graph_info = get_graph_info("skos")
-
-
-def test_nodes_number():
-    assert graph_info.nodes_num == 144
-
-
-def test_edges_number():
-    assert graph_info.edges_num == 252
-
-
-def test_labels():
-    assert graph_info.labels == {
-        "seeAlso",
-        "comment",
-        "subPropertyOf",
-        "rest",
-        "type",
-        "creator",
-        "inverseOf",
-        "label",
-        "description",
-        "unionOf",
-        "domain",
-        "range",
-        "example",
-        "scopeNote",
-        "contributor",
-        "title",
-        "subClassOf",
-        "definition",
-        "first",
-        "disjointWith",
-        "isDefinedBy",
-    }
+# graph_info = get_graph_info("skos")
+#
+#
+# def test_nodes_number():
+#     assert graph_info.nodes_num == 144
+#
+#
+# def test_edges_number():
+#     assert graph_info.edges_num == 252
+#
+#
+# def test_labels():
+#     assert graph_info.labels == {
+#         "seeAlso",
+#         "comment",
+#         "subPropertyOf",
+#         "rest",
+#         "type",
+#         "creator",
+#         "inverseOf",
+#         "label",
+#         "description",
+#         "unionOf",
+#         "domain",
+#         "range",
+#         "example",
+#         "scopeNote",
+#         "contributor",
+#         "title",
+#         "subClassOf",
+#         "definition",
+#         "first",
+#         "disjointWith",
+#         "isDefinedBy",
+#     }
 
 
 def test_raise_file_not_found_error():
